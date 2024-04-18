00de959a777989eb40fcd63c38b522c975dcd977
Matty Jorgensen <me@prplecake.com>
2024-04-18 17:53:36 -0500

    Add messy puts statements

--- _plugins/tag-generator.rb
+++ _plugins/tag-generator.rb
@@ -4,6 +4,7 @@ Jekyll::Hooks.register :posts, :post_write do |post|
                          .compact.map { |m| m[1] }
 
   tags = post['tags'].reject { |t| t.empty? }
+  puts "Generating tags..."
   generate_tags_files(tags)
 end
 
@@ -11,13 +12,17 @@ def generate_tags_files(tags)
   tags.each do |tag|
     slug = tag.downcase.strip.gsub(' ', '-').gsub(/[^\w-]/, '')
     # generate tag file
+    puts "Generating tag file for #{slug}..."
     if !File.exist?("_tags/#{slug}.md")
+      puts "Creating tag file for #{slug}..."
       File.open("_tags/#{slug}.md", "wb") do |file|
         file << "---\nlayout: tags\ntag-name: #{slug}\n---\n"
       end
     end
     # generate feed file
+    puts "Generating feed file for #{slug}..."
     if !File.exist?("_feeds/#{slug}.xml")
+      puts "Creating feed file for #{slug}..."
       File.open("_feeds/#{slug}.xml", "wb") do |file|
         file << "---\nlayout: feed\ntag-name: #{slug}\n---\n"
       end
