6e0267b442c014efe68bd32a835be5ce1ec4b242
Matty Jorgensen <me@prplecake.com>
2024-04-18 18:09:40 -0500

    Revert "Fix infinite loop"
    This reverts commit 614f2d318a1605539eea17b79d5f6045f7ba6fe0.

--- _plugins/tag-generator.rb
+++ _plugins/tag-generator.rb
@@ -12,20 +12,12 @@ def generate_tags_files(tags)
   tags.each do |tag|
     slug = tag.downcase.strip.gsub(' ', '-').gsub(/[^\w-]/, '')
     # generate tag file
-    puts "Generating tag file for #{slug}..."
-    if !File.exist?("_tags/#{slug}.md")
-      puts "Creating tag file for #{slug}..."
-      File.open("_tags/#{slug}.md", "wb") do |file|
-        file << "---\nlayout: tags\ntag-name: #{slug}\n---\n"
-      end
+    File.open("_tags/#{slug}.md", "wb") do |file|
+      file << "---\nlayout: tags\ntag-name: #{slug}\n---\n"
     end
     # generate feed file
-    puts "Generating feed file for #{slug}..."
-    if !File.exist?("_feeds/#{slug}.xml")
-      puts "Creating feed file for #{slug}..."
-      File.open("_feeds/#{slug}.xml", "wb") do |file|
-        file << "---\nlayout: feed\ntag-name: #{slug}\n---\n"
-      end
+    File.open("_feeds/#{slug}.xml", "wb") do |file|
+      file << "---\nlayout: feed\ntag-name: #{slug}\n---\n"
     end
   end
 end
