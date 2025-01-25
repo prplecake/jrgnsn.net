d346f16355b7db1f8a428171b5cc802a51caca6f
Matty Jorgensen <me@prplecake.com>
2025-01-25 14:58:06 -0600

    Fix GetPermalink date formats

--- src/jrgnsn.net.Web.Client/Utils/UrlUtils.cs
+++ src/jrgnsn.net.Web.Client/Utils/UrlUtils.cs
@@ -10,9 +10,9 @@ public static class UrlUtils
         return obj.GetType().Name switch
         {
             nameof(Post) =>
-                $"/{((Post)obj).PublishDate.Year}/{((Post)obj).PublishDate.Month}/{((Post)obj).PublishDate.Day}/{((Post)obj).Slug}",
+                $"/{((Post)obj).PublishDate:yyyy}/{((Post)obj).PublishDate:MM}/{((Post)obj).PublishDate:dd}/{((Post)obj).Slug}",
             nameof(TravelLog) =>
-                $"/travel/{((TravelLog)obj).StartDate.ToString("yyyy")}-{((TravelLog)obj).StartDate.ToString("MM")}-{((TravelLog)obj).StartDate.ToString("dd")}-{((TravelLog)obj).Slug}",
+                $"/travel/{((TravelLog)obj).StartDate:yyyy}-{((TravelLog)obj).StartDate:MM}-{((TravelLog)obj).StartDate:dd}-{((TravelLog)obj).Slug}",
             _ => throw new NotImplementedException()
         };
     }
